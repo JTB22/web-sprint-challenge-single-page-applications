824c8024fedc95b9d96e659477629ed3266f7c97
Josh Blood <jblood92@pm.me>
2023-07-03 14:45:48 -0400

    removed home link

--- src/Components/Pizza.js
+++ src/Components/Pizza.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const orderPizza = () => {
 
     return (
         <div>
             <h1>Order Pizza</h1>
-            <Link to="/">Home</Link>
         </div>
     )
 }
